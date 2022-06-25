36b65929e9125afaacb5a4a1aab93aa452a1a107
aaronlrv <aaronvitharanage@gmail.com>
2022-06-25 21:07:52 +0400

    Create gameboard function

---
+++ script.js
@@ -0,0 +1,36 @@
+let moduleFunctions = (function() {
+
+    function gameBoard (){
+        let board = ["x","x","x",
+                    "x", "x", "x",
+                    "x", "x" ,"x" ]
+        console.log(board)
+    }
+
+    return {
+        gameBoard
+    }
+})() 
+
+
+
+moduleFunctions.gameBoard()
+
+
+
+
+/// 3 modueles
+/// gameboard gameFlowCOntroller
+/// displayCOntroller
+
+///gameboard would contain the array, detection of player input 
+///gameflow would contain the turn system detection of player input may go here as well 
+/// displayCOntroller would update the displays of scores and the gameboard (updated array)SS
+/// convert array into board by creating a grid and then updating their content with markets in gameboard 
+
+/// only factory function
+/// players
+/// "get" and "set" helper functions could go under factories
+
+/// the whole "Start screen" "player selection" etc. can be spread out through multiple modules
+/// would not recommend throwing start screen whole button thing into a seperate module 
